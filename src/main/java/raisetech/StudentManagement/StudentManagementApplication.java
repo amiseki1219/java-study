93bf861d3f731439fd02d70617bdc1e70264dd2b
amiseki1219 <amisato.n1219@gmail.com>
2025-06-08 20:29:24 +0900

    Mapを使って生徒の登録・更新・一覧表示を実装

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,10 +1,13 @@
 package raisetech.StudentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,6 +16,7 @@ public class StudentManagementApplication {
 
   private String name = "Ami Seki";
   private String age = "26";
+  private Map<String,String> student;
 
   public static void main(String[] args) {
     SpringApplication.run(StudentManagementApplication.class, args);
@@ -33,6 +37,29 @@ public class StudentManagementApplication {
   public void updateStudentName(String name){
     this.name = name;
 
+  }
+  @PostMapping("/register")
+  public String registerStudent(@RequestParam String name, @RequestParam String age){
+    if (this.student == null){
+      this.student = new HashMap<>();
+    }
+    student .put(this.name,this.age);
+    return "登録完了：" + name + "(" + age + "歳)";
+  }
+  @PostMapping("/updateStudent")
+  public String updateStudentAge(@RequestParam String name,@RequestParam String newAge){
+    if(student == null || !student.containsKey(name)){
+      return name + "はまだ登録できていません";
+    }
+    student.put(name,newAge);
+    return name + "の年齢を" + newAge + "に更新しました";
+  }
+  @GetMapping("/students")
+  public Map<String,String> getAllstudent(){
+    if(student == null){
+      return Map.of();
+    }
+    return student;
   }
 }
 
