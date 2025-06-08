ca0619b6e4e1164feec6c0946be21ac0f487b49f
amiseki1219 <amisato.n1219@gmail.com>
2025-06-08 19:30:17 +0900

    初回コミット

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,13 +1,38 @@
 package raisetech.StudentManagement;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StudentManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementApplication.class, args);
-	}
+  private String name = "Ami Seki";
+  private String age = "26";
 
+  public static void main(String[] args) {
+    SpringApplication.run(StudentManagementApplication.class, args);
+  }
+
+  @GetMapping("/studentInfo")
+  public String getStudentInfo() {
+    return name + " " + age + "歳";
+  }
+
+  @PostMapping("/studentInfo")
+  public void setStudentInfo(String name,String age){
+    this.name = name;
+    this.age = age;
+  }
+
+  @PostMapping("/studentName")//nameだけ変更したい
+  public void updateStudentName(String name){
+    this.name = name;
+
+  }
 }
+
