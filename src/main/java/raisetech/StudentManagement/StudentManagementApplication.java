c7b15329c92a40bc798fd0b74d46560f2ca28c89
amiseki1219 <amisato.n1219@gmail.com>
2025-06-30 05:20:57 +0900

    students テーブルを作成し、5件のデータをINSERT
    students_courses テーブルを作成し、5件のデータをINSERT（講座内容＋期間付き）
    
    Studentクラスを作成（Lombok使用）
    
    StudentCourseクラスを作成（Lombok＋Timestamp対応）
    
    MyBatisで StudentRepository 作成 → /studentList エンドポイント実装
    
    MyBatisで StudentCourseRepository 作成 → /studentCourseList エンドポイント実装
    
    application.properties の datasource・MyBatis設定修正（camel-case対応）
    
    文字化け調査（IntelliJのターミナル結果 vs curlブラウザ出力の違い）
    
    タイムゾーン（UTC→JST）に修正して日本時間で出力されるように対応

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,13 +1,35 @@
 package raisetech.StudentManagement;
 
+import java.util.List;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@MapperScan("raisetech.StudentManagement")
+@RestController
 public class StudentManagementApplication {
+
+	@Autowired
+	private StudentRepository repository;
+	@Autowired
+	private StudentCourseRepository courseRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
+	}
+
+	@GetMapping("/studentList")
+	public List<Student> getStudentList(){
+		return repository.search();
+	}
+
+	@GetMapping("/studentCourseList")
+	public List<StudentCourse> getCourseList(){
+		return courseRepository.findAll();
 	}
 
 }
