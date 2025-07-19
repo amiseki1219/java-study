54e135befd6f075dc0d453be4bbb30cef9f030b8
amiseki1219 <amisato.n1219@gmail.com>
2025-07-19 17:10:24 +0900

    コード全体のリファクタリング

--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -10,48 +10,79 @@ import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
 
 /**
- * 受講生情報を扱うリポジトリ。
- *
- * 全件検索や単一条件での検索が行えるクラスです。
+ * 受講生テーブルと受講生コース情報テーブルと紐づくRepositoryです。
  */
 @Mapper
 public interface StudentRepository {
 
   /**
-   * 全件検索します。
-   *
-   * @return 全件検索した受講生情報の一覧
+   * 受講生の全件検索を行います。
+   * @return 受講生一覧（全件）
    */
-
   @Select("SELECT * FROM students")
   List<Student> search();
 
-  @Select("SELECT * FROM students_courses")
-  List<StudentsCourses> searchStudentsCoursesList();
 
-
+  /**
+   * 受講生の検索を行います。
+   * @param id　受講生ID
+   * @return 受講生
+   */
   @Select("SELECT * FROM students WHERE id = #{id}")
   Student searchStudent(int id);
 
-  @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
-  List<StudentsCourses> searchStudentsCourses(int id);
 
+  /**
+   *受講生のコース情報の全件検を行います。
+   * @return 受講生のコース情報（全件）
+   */
+  @Select("SELECT * FROM students_courses")
+  List<StudentsCourses> searchStudentCourseList();
+
+
+  /**
+   * 受講生IDに紐づく受講生コース情報を検索します。
+   * @param id　受講生ID
+   * @return 受講生IDに紐づく受講生コース情報
+   */
+  @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
+  List<StudentsCourses> searchStudentCourse(int id);
+
+  /**
+   * 受講生を新規登録します。
+   * IDに関しては自動採番を行う。
+   * @param student　受講生
+   */
   @Insert("INSERT INTO students(name, kana_name, nickname, email, area, age, sex, remark, is_deleted)" + "VALUES (#{name}, #{kanaName}, #{nickname}, #{email}, #{area}, #{age}, #{sex}, #{remark}, false)")
   @Options(useGeneratedKeys = true, keyProperty = "id")
   void registerStudent(Student student);
 
+  /**
+   * 受講生コース情報を新規登録します。
+   * IDに関しては自動採番を行う。
+   * @param studentsCourse 受講生コース情報
+   */
   @Insert("INSERT INTO students_courses (student_id, course_name, course_start_at, course_end_at) " +
       "VALUES (#{studentId}, #{courseName}, #{courseStartAt}, #{courseEndAt})")
   @Options(useGeneratedKeys = true, keyProperty = "id")
-  void registerStudentCourse(StudentsCourses studentsCourses);
+  void registerStudentCourse(StudentsCourses studentsCourse);
 
 
+  /**
+   *受講生を更新します。
+   * @param student 受講生
+   */
 
   @Update("UPDATE students SET name = #{name}, kana_name = #{kanaName}, nickname = #{nickname}, email = #{email}, area = #{area}, age = #{age}, sex = #{sex}, remark = #{remark}, is_deleted = #{isDeleted} WHERE id = #{id}")
   void updateStudent(Student student);
 
+  /**
+   *受講生コーヅ情報のコース名を更新します。
+   * @param studentsCourse　受講生コース情報
+   */
   @Update("UPDATE students_courses SET course_name = #{courseName} WHERE id = #{id}")
-  void updateStudentCourses(StudentsCourses studentsCourses);
+  void updateStudentCourses(StudentsCourses studentsCourse);
 
 }
+
 
